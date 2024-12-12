58a38259f9eef42157f13cceb3bb8d4edfaee856
Mythrayan2231 <mythrayan3103@gmail.com>
2024-12-13 01:29:33 +0530

    first commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './acc.png'
+import shopify from './cog.png'
+import starbucks from './nsic.png'
+import tesla from './black.png'
+import logo from './logo.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,177 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: " Full Stack Web Development",
+        company_name: "National Small Industries Corporation Ltd.",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "june-2024",
+        points: [
+            "Developed and implemented web applications using full-stack technologies, focusing on both front-end and back-end development",
+            "Collaborated with senior developers to design user interfaces and improve user experience, ensuring responsiveness and functionality.",
+            " Gained hands-on experience with full-stack web development and enhanced my skills in scalability. ",
+        ],
+    },
+    {
+        title: "Frontend Web Development",
+        company_name: "Black Arrow Data Matrix Pvt Ltd",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "july 2023 - August 2023",
+        points: [
+            "Assisted in the development of user-friendly interfaces for web applications, utilizing HTML, CSS, and JavaScript frameworks.",
+            "Participated in code reviews and collaborated with cross-functional teams to ensure alignment with project goals and timelines.",
+        ],
+    },
+    {
+        title: "UI/UX",
+        company_name: "Cognifyz Technologies",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Completed UI/UX design intern in cognifyz technologies.",
+            "This company gave varies task  during the intern period it helps to gain some knowledge about what is UI/UX.",
+            "Gained hands-on experience with UI/UX design and enhanced my skills in scalability.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Data Analytics and Visualization Job Simulation",
+        company_name: "Accenture",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "october 2024",
+        points: [
+            "Completed a simulation focused on advising a hypothetical social media client as a Data Analyst at Accenture.",
+            "Cleaned, modelled and analyzed 7 datasets to uncover insights into content trends to inform strategic decisions.",
+            "Prepared a PowerPoint deck and video presentation to communicate key insights for the client and internal stakeholders.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Mythrayan2231',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/mythrayan-n-p-37576a229/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'RefineHub',
+        description: 'Developed a Full Stack application that has a Dashboard, Properties and Agents , helping users find the best deals.',
+        link: 'https://github.com/Mythrayan2231',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: ' Thermal Facial Recognition for Automated Attendance',
+        description: 'Designed and developed an AI-driven system utilizing thermal image processing to automatically recognize student faces or attendance tracking.',
+        link: 'https://github.com/Mythrayan2231',
+    },
+];
